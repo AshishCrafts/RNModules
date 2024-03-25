f54530e4de006b39d6f2a7df69971079cace85c0
jinnKazama <ashishdevsamurai@gmail.com>
2024-03-25 21:27:52 +0530

    dev/home

--- App/screen/home/Home.js
+++ App/screen/home/Home.js
@@ -2,22 +2,98 @@ import {hp, wp} from '../../services/diamension';
 import React from 'react';
 import {InputText, RNButton} from '../../components';
 import {colors} from '../../themes';
-import {View, Text, StyleSheet} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  FlatList,
+  Image,
+} from 'react-native';
 
 const HomeScreen = ({navigation}) => {
   const handleSubmit = async () => {
     navigation.navigate('Chat');
   };
+
+  const DATA = [
+    {
+      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+      Name: 'Ashish Singh',
+      CallDuragtion: '09:35 PM',
+      Time: '01:00',
+    },
+    {
+      id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+      Name: 'Superman',
+      CallDuragtion: '09:35 PM',
+      Time: '01:00',
+    },
+    {
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      Name: 'Golden',
+      CallDuragtion: '09:35 PM',
+      Time: '01:00',
+    },
+    {
+      id: 'bd7acbea-c1b1-46c2-aed5-3ab28ba',
+      Name: 'Lucky',
+      CallDuragtion: '09:35 PM',
+      Time: '01:00',
+    },
+    {
+      id: '3ac68afc-c605-48d3-a4f8-fbd91aa63',
+      Name: 'Lovely',
+      CallDuragtion: '09:35 PM',
+      Time: '01:00',
+    },
+    {
+      id: '58694a0f-3da1-471f-bd96-145579d72',
+      Name: 'Golden',
+      CallDuragtion: '09:35 PM',
+      Time: '01:00',
+    },
+  ];
+
+  const Item = ({item}) => (
+    <View style={styles.cardContainer}>
+      <View style={styles.cardContent}>
+        <View style={styles.leftContent}>
+          <Text style={styles.name}>{item.Name}</Text>
+          <Text style={styles.duration}>{item.CallDuragtion}</Text>
+        </View>
+        <View style={styles.rightContent}>
+          <Text style={styles.time}>{item.Time}</Text>
+        </View>
+      </View>
+    </View>
+  );
   return (
     <View style={styles.mainContainer}>
-      <Text style={styles.title}>Home Screen</Text>
+      <Text style={styles.title}>Recent Call</Text>
 
-      <Text>You want to create your account?</Text>
-      <View style={styles.bottomText}>
-        <Text style={styles.loginText}>Sign up</Text>
-      </View>
+      <Image
+        source={{
+          uri: 'https://www.tech101.in/wp-content/uploads/2019/09/xxtech101-qr.png,Mic.1Ef0vXWtoj.png.pagespeed.ic.Pb478GychK.webp',
+        }}
+        style={{
+          width: '80%',
+          borderRadius: 9,
+          height: 250,
+        }}
+      />
+
+      <SafeAreaView style={{flex: 1}}>
+        <FlatList
+          data={DATA}
+          showsVerticalScrollIndicator={false}
+          renderItem={({item}) => <Item item={item} />}
+          keyExtractor={item => item.id}
+        />
+      </SafeAreaView>
+
       <RNButton
-        title={'Login'}
+        title={'Chat Message '}
         buttonTextColor={styles.btnText}
         buttonStyle={{
           width: wp(70),
@@ -34,7 +110,7 @@ const HomeScreen = ({navigation}) => {
 const styles = StyleSheet.create({
   mainContainer: {
     flex: 1,
-    paddingVertical: hp(12),
+    paddingVertical: hp(2),
     alignItems: 'center',
     backgroundColor: '#fff',
   },
@@ -43,16 +119,41 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginBottom: 20,
   },
-  formWrapper: {
-    width: '80%',
-  },
-  card: {
-    backgroundColor: 'white',
-    borderRadius: 8,
-    paddingVertical: 45,
-    paddingHorizontal: 25,
-    width: '100%',
+  cardContainer: {
     marginVertical: 10,
+    borderRadius: 10,
+    backgroundColor: colors.light_gray,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  cardContent: {
+    width: '90%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 20,
+  },
+  rightContent: {
+    // flex: 0.3,
+    alignItems: 'flex-end',
+  },
+  name: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  duration: {
+    fontSize: 14,
+    color: '#777777',
+  },
+  time: {
+    fontSize: 14,
+    fontWeight: 'bold',
   },
 });
 
